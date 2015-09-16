6618d95aaf5c80976e63cb9986faf68207d15d7c
Paul <shpytalenko@gmail.com>
2015-09-16 18:32:44 +0300

    Update drive.rb

--- drive/drive.rb
+++ drive/drive.rb
@@ -45,7 +45,7 @@ def setup()
     flow = Google::APIClient::InstalledAppFlow.new(
       :client_id => client_secrets.client_id,
       :client_secret => client_secrets.client_secret,
-      :scope => ['https://www.googleapis.com/auth/drive']
+      :scope => ['https://www.googleapis.com/auth/drive',"https://spreadsheets.google.com/feeds/"]
     )
     client.authorization = flow.authorize(file_storage)
   else
